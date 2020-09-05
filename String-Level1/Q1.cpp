fdda9784935ecf36c01a2758b22bc0f46851a09a
Gaurav <gauravtewari24@gmail.com>
2020-09-05 09:14:17 +0530

    initial

---
+++ String-Level1/Q1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define f(i, a, b) for (ll i = a; i < b; i++)
+
+int main()
+{
+    ll t, n, a = 0, b = 0, i, j, k, c, sum;
+    string s;
+    cin >> s;
+    cout << s;
+}
---
+++ String-Level1/Q2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define f(i, a, b) for (ll i = a; i < b; i++)
+
+int main()
+{
+    ll t, n, a = 0, b = 0, i, j, k, c, sum;
+    string f, l, m;
+    cin >> f >> m >> l;
+    cout << f << " " << m << " " << l << "- " << f[0] << "." << m[0] << "." << l[0];
+}
